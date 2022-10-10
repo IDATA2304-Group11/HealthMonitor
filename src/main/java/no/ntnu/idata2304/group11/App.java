8add7cc1fdb2e651d37ff87c3815d5506733bfd7
Jørgen Finsveen <joergen.finsveen@gmail.com>
2022-10-10 11:07:26 +0200

    initial commit

---
+++ src/main/java/no/ntnu/idata2304/group11/App.java
@@ -0,0 +1,13 @@
+package no.ntnu.idata2304.group11;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
