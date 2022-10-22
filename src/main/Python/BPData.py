850c5ff7ee252fd28be71a713a199d66851a6377
Edvardsen <petter-edvardsen@hotmail.com>
2022-10-22 14:27:32 +0100

    Finalized generation of realistic data
    Data generated is now stable and pseudorandom.

--- src/main/Python/BPData.py
+++ src/main/Python/BPData.py
@@ -10,46 +10,47 @@
 
 # Template based on data from "National Health and Nutrition Examination Survey 2007-2010. JAMA. 2011;305(19):1971-1979"
 
-
 # chance of having a increase during a positive phase
 
 import random
 import numpy as np
 import matplotlib.pyplot as plt
 
-# The average standard deviation of adults
-STANDARD_DEVIATION = 13
-
 """
 This class represents a person with a given gender,age, incident_probability and blood pressure fluctuation
 """
 
+# The average standard deviation of adults
+STANDARD_DEVIATION = 13
+# The average fluctuation percentage in BP in adult patients
+FLUCTUATION_PERCENTAGE = 0.20
+
 
 class Patient:
 
-    def __init__(self, gender, age, incident_probability, fluctuation):
+    def __init__(self, gender, age, incident_probability, fluctuationRange):
         self.gender = gender
         self.age = age
         self.incident_probability = incident_probability
-        self.fluctuation = fluctuation
+        self.fluctuation = fluctuationRange
 
     gender = int
     age = int
     incident_probability = float
-    fluctuation = float
+    fluctuationRange = int
 
 
-# Generates a psuedorandom elderly person
-#
-def generatePerson():
-    gender = random.randrange(0, 1)
+# Generates a psuedorandom elderly person with its respective attributes
+def generatePatient():
+    gender = random.randrange(0, 2)
     age = random.randrange(60, 80)
-    incident_probability = random.uniform(0.0, 0.001);
-    fluctuation = random.uniform(0, 0.09)
+    incident_probability = random.uniform(0.005, 0.01)
+    fluctuationRange = round(FLUCTUATION_PERCENTAGE * fetchMeanValue(age, gender))
 
-    return Patient(gender, age, incident_probability, fluctuation)
+    return Patient(gender, age, incident_probability, fluctuationRange)
 
 
+# Fetches the mean value of a given age and gender of a patient
 def fetchMeanValue(age, gender):
     if gender == 0:
         return fetchMeanValuesMan(age)
@@ -57,62 +58,101 @@ def fetchMeanValue(age, gender):
         return fetchMeanValuesFemale(age)
 
 
-# Returns the male mean value in systolic and diastolic blood pressure of a given age
+# Returns the male mean value in blood pressure of a given age
+# Values are extracted from "National Health and Nutrition Examination Survey 2007-2010. JAMA. 2011;305(19):1971-1979"
 def fetchMeanValuesMan(age):
     if age < 64:
-        return 130, 80
+        return 105
     elif age < 69:
-        return 135, 85
+        return 110
     elif age < 74:
-        return 150, 95
+        return 123
     elif age < 79:
-        return 170, 105
+        return 138
     else:
-        return 180, 110
+        return 145
 
 
 # Returns the female mean value in systolic and diastolic blood pressure of a given age
+# Values are extracted from "National Health and Nutrition Examination Survey 2007-2010. JAMA. 2011;305(19):1971-1979"
 def fetchMeanValuesFemale(age):
     if age < 64:
-        return 130, 80
+        return 105
     elif age < 69:
-        return 135, 85
+        return 110
     elif age < 74:
-        return 140, 90
+        return 115
     else:
-        return 145, 95
+        return 120
 
 
+# Creates a sine value with given parameters
+def customizableSineWave(variable, amplitude, frequency, equilibrium):
+    return amplitude * np.sin(frequency * variable) + equilibrium
 
 
-def customizableSineWave(amplitude, phase, equilibrium):
+# Returns a random integer in the range 0 - 1, a bit.
+def getRandomBit():
+    return random.randrange(0, 1)
 
+
+# Returns a coefficient sign value where the probability for growth towards the equilibrium line is favored
+def stabilizeAmplitude(amplitude, amplitudeRange):
+    coefficientSign = 1
+
+    if amplitude > amplitudeRange / 2:  # Amplitude is in upper quadrant of fluctuation range
+        coefficientSignValue = random.randrange(0, 3)
+
+        if coefficientSignValue > 0:
+            coefficientSign = -1
+        else:
+            coefficientSign = 1;
+
+    if amplitude < -1 * (amplitudeRange / 2):
+        coefficientSignValue = random.randrange(0, 3)
+
+        if coefficientSignValue > 0:
+            coefficientSign = 1
+        else:
+            coefficientSign = -1
+
+    return coefficientSign
 
 
 # Generates bloodPressure for a given time interval
-def generateBPDataMinutes(time):
-    person = generatePerson()
+def generateBPDataSeconds(seconds):
+    person = generatePatient()
 
-    equilibrium_line = fetchMeanValue(person.age, person.gender)[0]
+    equilibrium_line = fetchMeanValue(person.age, person.gender)
+    fluctuationRange = person.fluctuation
+    time_interval = np.arange(1, seconds, 1)
+    amplitude = fluctuationRange / 2
+    # frequency = random.randrange(60, 100)
 
-    frequency = person.fluctuation
+    BPdata = []
 
-    time_interval = np.linspace(1, 600, 100)
+    for t in time_interval:
 
-    interval = np.arange(0, 600, 1)
+        coefficient = stabilizeAmplitude(amplitude, fluctuationRange)
 
-    amplitude = STANDARD_DEVIATION / equilibrium_line
+        amplitude = coefficient * random.randrange(0, fluctuationRange)
 
-    half_phase = np.pi / frequency # takes the last entry as the next wave's equilibrium
+        dataValue = customizableSineWave(t, amplitude, 1, equilibrium_line)
 
+        BPdata.append(dataValue)
 
+    # Normal heart rate 60 - 100, so 60 to 100 phases in 60 iterations,  freq bigger than x so more than 60 phases a min
+    # Vary in amplitude to generate realistic data, + and - in the range of SD
 
-    plt.plot(time_interval, 13 * np.sin(time_interval) + equilibrium_line)
-
-
-    plt.xlabel("Time")
-    plt.ylabel("SDB")
+    plt.plot(time_interval, BPdata)
     plt.show()
+    print(person.age)
+    if person.gender == 0:
+        print("Male")
+    else:
+        print("Female")
 
 
-generateBPDataMinutes(20)
+    return BPdata, person.gender, person.age;
+
+generateBPDataSeconds(50)
