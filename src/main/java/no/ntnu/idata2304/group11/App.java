d64562ce9646d9427f5e0b9017b856c8a4b2e3bd
DESKTOP-QV5T0ON\Oledo <okd@stud.ntnu.no>
2022-10-25 13:53:33 +0200

    Deleted the class ClientTCP. Workt on the class Mqtt. Created sendMassage(String line) in the Mqtt class. Rewiten the constructor to Mqtt(String address, String port, String id). Made it posible to connect and disconet from the broker. Can sand masseg and resive comformation that the broker resved the massage. Created new class OnMassageCallback().

--- src/main/java/no/ntnu/idata2304/group11/App.java
+++ src/main/java/no/ntnu/idata2304/group11/App.java
@@ -1,18 +1,17 @@
 package no.ntnu.idata2304.group11;
 
-import no.ntnu.idata2304.group11.Client.ClientTCP;
-import no.ntnu.idata2304.group11.ui.UI;
+import no.ntnu.idata2304.group11.Client.Mqtt;
+import org.eclipse.paho.client.mqttv3.MqttException;
+
+import java.io.IOException;
 
 /**
  * Class starts the main application.
  */
 public class App {
 
-    public static void main(String[] args) {
-        //UI ui = new UI();
-        //ui.start();
-        ClientTCP client = new ClientTCP("1.241.152.12", 1883);
-        
+    public static void main(String[] args) throws MqttException, IOException {
+        Mqtt mqtt = new Mqtt("129.241.152.12","1883","102");
     }
 
 }
--- src/main/java/no/ntnu/idata2304/group11/Client/Mqtt.java
+++ src/main/java/no/ntnu/idata2304/group11/Client/Mqtt.java
@@ -1,10 +1,12 @@
 package no.ntnu.idata2304.group11.Client;
 
-import org.eclipse.paho.client.mqttv3.IMqttClient;
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-import org.eclipse.paho.client.mqttv3.MqttException;
-import java.util.UUID;
+import org.eclipse.paho.client.mqttv3.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static java.lang.System.in;
 
 /**
  * This class represent the MQTT Protocol. It is used to connect the microcontrollers and sensor to the cloud.
@@ -12,22 +14,100 @@ import java.util.UUID;
  * @version 1.0
  */
 public class Mqtt {
-    //Gives a random UUID to the new client.
-    private String id = UUID.randomUUID().toString();
-    //The broker host on IP 129.241.152.12 and on Port 1883.
-    private String broker = "tcp://129.241.152.12:1883";
-    //Sets up the client.
-    private IMqttClient client = new MqttClient(broker, id);
-    //
-    private MqttConnectOptions options = new MqttConnectOptions();
 
-    public Mqtt() throws MqttException {
-        //Will automatically try to reconnect to the server in the event of a network failure.
-        options.setAutomaticReconnect(true);
-        //Will discard unset messages from a previous run.
-        options.setCleanSession(true);
-        //Connection timeout is set to 10 seconds.
-        options.setConnectionTimeout(10);
-        client.connect(options);
+    //Different MQTT variables.
+    private String id;
+    private String broker;
+    private IMqttClient client;
+    private MqttConnectOptions options;
+
+    //Input function
+    private BufferedReader input;
+
+    //Topics
+    private int qos = 0;
+    private String pubTopic = "testtopic/1";
+    private String subTopic = "testtopic/#";
+
+    /**
+     * Constructor of the MQTT Client.
+     * @param address address to the broker you want to connect to.
+     * @param port port of the broker you want to connect to.
+     * @param id the id of the one connecting.
+     * @throws MqttException
+     * @throws IOException
+     */
+    public Mqtt(String address, String port, String id) throws MqttException, IOException {
+
+        //Sets up the client connection
+        this.id = id;
+        this.broker = "tcp://" + address + ":" + port;
+        this.client = new MqttClient(this.broker, this.id);
+        this.options = new MqttConnectOptions();
+
+        //TODO Fine out what options we want on the Client.
+        //Setts up different options for the Client
+        this.options.setAutomaticReconnect(true);
+        this.options.setCleanSession(true);
+        this.options.setCleanSession(true);
+        this.options.setConnectionTimeout(5);
+
+        //Sets up input function
+        this.input = new BufferedReader(new InputStreamReader(in));
+
+        //Set callback
+        this.client.setCallback(new OnMassageCallback());
+
+        //Establish connection
+        System.out.println("Connecting to broker: " + this.broker);
+        this.client.connect(options);
+
+        //TODO find out what topic i want to sub to.
+        //subscribe to a toppic
+        this.client.subscribe(subTopic);
+
+
+        //TODO find out how the programe can tel the user that it didnt connect to the broker, insted of the program crashing.
+        //Checks if the client is connected to the broker.
+        String line = "";
+        if(this.client.isConnected() == true) {
+            System.out.println("Connected to the broker: " + this.broker);
+
+            //TODO Find out why when "Disconnect" it sends the masseg to the broker.
+            while(!line.equals("Disconnect")) {
+                line = this.input.readLine();
+                sendMessage(line);
+            }
+
+            System.out.println("Disconnecting");
+            this.client.disconnect();
+            this.input.close();
+        }
+        else {
+            System.out.println("Did not connect to the broker: " + this.broker);
+            this.client.disconnect();
+            this.input.close();
+        }
+    }
+
+    /**
+     * Sends message to the broker
+     * @param line the message you want to send
+     */
+    private void sendMessage(String line){
+        try {
+            if(!this.client.isConnected()) {
+                return;
+            }
+            MqttMessage message = new MqttMessage(line.getBytes());
+            message.setQos(qos);
+            client.publish(this.pubTopic, message);
+        }
+        catch (MqttPersistenceException e) {
+            e.printStackTrace();
+        }
+        catch (MqttException e) {
+            e.printStackTrace();
+        }
     }
 }
